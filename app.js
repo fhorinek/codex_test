02c38ac2ab5dd07b7d044270dcb4a34a91dc71f6
František Horínek <frantisekhorinek@gmail.com>
2026-02-07 20:15:04 +0100

    Hide unassigned column when empty

--- app.js
+++ app.js
@@ -205,8 +205,11 @@ function buildKanban() {
       unassigned.push(task);
     }
   });
-  const orderedStates = [...states, "!unassigned"];
-  tasksByState.set("!unassigned", unassigned);
+  const orderedStates = [...states];
+  if (unassigned.length) {
+    orderedStates.push("!unassigned");
+    tasksByState.set("!unassigned", unassigned);
+  }
   orderedStates.forEach((stateTag) => {
     const column = document.createElement("div");
     column.className = "kanban-column";
